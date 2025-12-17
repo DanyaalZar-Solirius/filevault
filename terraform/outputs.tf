dac2d8eb96b993d5ea17bc24131e0a58f401d86b
DanyaalZar-Solirius <danyaal.zar@solirius.com>
2025-12-17 17:14:45 +0000

    Separate repos

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "vault_app_repo_url"{
-    value = aws_ecr_repository.vault_app_repo.repository_url
+output "vault_app_frontend_repo_url"{
+    value = aws_ecr_repository.vault_app_frontend_repo.repository_url
+}
+
+output "vault_app_backend_repo_url" {
+    value = aws_ecr_repository.vault_app_backend_repo.repository_url
 }
