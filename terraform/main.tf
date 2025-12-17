dd9ffbda5b7d579f1110d658bda078699bb2d40c
DanyaalZar-Solirius <danyaal.zar@solirius.com>
2025-12-17 17:16:33 +0000

    Create docker files
    * Dockerfiles
    
    * Repo url for terraform to output
    
    * Dockerignore for backend
    
    * Fix dockerisation of frontend
    
    * Separate repos

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,20 @@ resource "aws_s3_bucket" "filevault_bucket" {
   }
 }
 
-// ECR Repo
-resource "aws_ecr_repository" "vault_app_repo" {
-  name                 = var.ecr_repo_name         
-  image_tag_mutability = "IMMUTABLE"    
-         
+// ECR Repos
+resource "aws_ecr_repository" "vault_app_frontend_repo" {
+  name                 = "${var.ecr_repo_name}-frontend"         
+  image_tag_mutability = "MUTABLE"    
+
+  image_scanning_configuration {
+    scan_on_push = true                        
+  }
+}
+
+resource "aws_ecr_repository" "vault_app_backend_repo" {
+  name                 = "${var.ecr_repo_name}-backend"         
+  image_tag_mutability = "MUTABLE"    
+
   image_scanning_configuration {
     scan_on_push = true                        
   }
