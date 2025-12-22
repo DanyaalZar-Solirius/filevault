f8eb8919a1bad720168b124258bf2e9385c7b3c6
DanyaalZar-Solirius <danyaal.zar@solirius.com>
2025-12-22 11:53:32 +0000

    reduce to one node group

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -13,10 +13,6 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
 
   eks_managed_node_groups = {
     one = {
@@ -27,16 +23,6 @@ module "eks" {
       min_size     = 1
       max_size     = 3
       desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
     }
   }
 }
